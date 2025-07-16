a2ebbddf8929d6c8c18629abceb611518fa7a1e6
Jessie Smith <jrs201@cam.ac.uk>
2025-07-16 10:58:08 +0100

    adding image to the home page

--- aviation.py
+++ aviation.py
@@ -1,6 +1,6 @@
 #inputs
 days_per_year = 365.0
-passenger_per_year = 4.46E9
+passengers_per_year = 4.46E9
 flights_per_day = 2.0
 seats = 150.0
 
@@ -8,5 +8,6 @@ seats = 150.0
 passengers_per_day =  passengers_per_year/days_per_year
 aircraft_per_day = passengers_per_day/(seats*flights_per_day)
 
+#print output
 print(f"{passengers_per_day=:.2e}")
 print(f"{aircraft_per_day=:.2e}")
