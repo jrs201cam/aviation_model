760ef1551b2696da66b757e7546278b01d3f2dc9
Jessie Smith <jrs201@cam.ac.uk>
2025-07-17 12:17:42 +0100

    fixing ruff linting errors

--- src/aviation/__init__.py
+++ src/aviation/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ("passengers_per_day", "aircraft_per_day")
 
-from aviation.aviation import passengers_per_day, aircraft_per_day
+from aviation.aviation import aircraft_per_day, passengers_per_day
