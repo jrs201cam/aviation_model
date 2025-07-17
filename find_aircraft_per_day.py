15f99e4173abc194754db73360b60c82d3904492
Jessie Smith <jrs201@cam.ac.uk>
2025-07-17 10:03:12 +0100

    created functions and new files per scripts

---
+++ find_aircraft_per_day.py
@@ -0,0 +1,12 @@
+#inputs
+days_per_year = 365.0
+passengers_per_year = 4.46E9
+flights_per_day = 2.0
+seats = 150.0
+
+#model
+passengers_per_day =  passengers_per_year/days_per_year
+aircraft_per_day = passengers_per_day/(seats*flights_per_day)
+
+#print output
+print(f"{aircraft_per_day=:.2e}")
