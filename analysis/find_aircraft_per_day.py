75e5a6df4ed69d87a07558cd8478f18c6567b71f
Jessie Smith <jrs201@cam.ac.uk>
2025-07-23 11:04:11 +0100

    adding engine

--- analysis/find_aircraft_per_day.py
+++ analysis/find_aircraft_per_day.py
@@ -2,20 +2,24 @@
 
 # import function file
 import aviation
+from aviation import _engine as engine
 
 # inputs
-days_per_year = 365.0
+days_per_year = 365.25
 passengers_per_year = 4.46e9
 flights_per_day = 2.0
 seats = 150.0
 
-# model
-passengers_per_day = aviation.passengers_per_day(
-    passengers_per_year=passengers_per_year, days_per_year=days_per_year
-)
-aircraft_per_day = aviation.aircraft_per_day(
-    passengers_per_day=passengers_per_day, seats=seats, flights_per_day=flights_per_day
-)
+inputs = {
+    "passengers_per_year": passengers_per_year,
+    "days_per_year": days_per_year,
+    "seats": seats,
+    "flights_per_day": flights_per_day,
+}
 
-# print output
+output = "aircraft_per_day"
+
+systems_model = engine.SystemsModel(aviation.transforms)
+aircraft_per_day = systems_model.evaluate(inputs, output)
+
 print(f"{aircraft_per_day=:.2e}")
